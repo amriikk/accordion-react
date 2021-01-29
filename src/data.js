fea0dfd1ee22bcee4a7b9a297afeeb26795286d9
John Trujillo <amriikk@gmail.com>
2021-01-29 01:22:05 -0800

    update seeded data

--- src/data.js
+++ src/data.js
@@ -3,43 +3,43 @@ const questions = [
       id: 1,
       title: 'What online resources do you use to help you do your job?',
       info:
-        'Most IT workers turn to websites such as StackExchange or Github when they need help with something. Serious professionals will have their own selection of websites, online communities, social media feeds and other resources specific to their interests. The answer to this question will give you an indication of how engaged the candidate is with the broader IT world.  vinyl poutine brooklyn, next level direct trade iceland. Shaman copper mug church-key coloring book, whatever poutine normcore fixie cred kickstarter post-ironic street art.',
+        'Most IT workers turn to websites such as StackExchange or Github when they need help with something. Serious professionals will have their own selection of websites, online communities, social media feeds and other resources specific to their interests. The answer to this question will give you an indication of how engaged the candidate is with the broader IT world.',
     },
     {
       id: 2,
       title: 'How do you keep your technology skills current?',
       info:
-        'Coloring book forage photo booth gentrify lumbersexual. Migas chillwave poutine synth shoreditch, enamel pin thundercats fashion axe roof party polaroid chartreuse.',
+        'Tech professionals work hard to keep their knowledge base current by reading blogs and forums, taking online courses, joining hackathons and plugging away at personal IT projects. This tech interview question can help you gauge the candidate’s enthusiasm for the profession, as well as open up a conversation about professional development.',
     },
     {
       id: 3,
-      title: 'What is BankID?',
+      title: 'Pretend I’m not a tech person. Can you explain [a relevant technology] in simple terms?',
       info:
-        'Enamel pin fam sustainable woke whatever venmo. Authentic asymmetrical put a bird on it, lumbersexual activated charcoal kinfolk banjo cred pickled sartorial.',
+        'IT plays a crucial role in almost every company, so the ability to communicate with non-technical people is a must. You can assess candidates’ communication skills with this IT interview question. Do they avoid obscure acronyms and jargon? How well can they break down a complicated process? Try asking a few “dumb” follow-up questions to get a sense of how they’d interact with non-tech colleagues.',
     },
     {
       id: 4,
-      title: 'Whose birth number can I use?',
+      title: 'What strengths do you think are most important in a developer [or another relevant IT position]?',
       info:
-        'Edison bulb direct trade gentrify beard lo-fi seitan sustainable roof party franzen occupy squid. Knausgaard cronut succulents, scenester readymade shabby chic lyft. Copper mug meh vegan gentrify.',
+        'A question like this can reveal how the interviewee feels about the position and what they think they would bring to it. Some candidates may focus on technical abilities and IT certifications, while others may talk more about problem solving, attention to detail, communication and other general job skills. Look for IT candidates who give a balanced answer. ',
     },
     {
       id: 5,
-      title: 'When do I recieve a password ordered by letter?',
+      title: 'What three words would your colleagues use to describe you?',
       info:
-        'Locavore franzen fashion axe live-edge neutra irony synth af tilde shabby chic man braid chillwave waistcoat copper mug messenger bag. Banjo snackwave blog, microdosing thundercats migas vaporware viral lo-fi seitan ',
+        'The answer can clue you in to a candidate’s personality trait that may not be readily apparent through their resume or traditional interview questions. It also gives insight into how the individual perceives themselves and the role they’re applying for. For example, if their answer focuses on their creative side but the position is very analytical in nature, the job may not be a good fit.',
     },
     {
         id: 6,
-        title: 'Do I have to allow the use of cookies?',
+        title: 'Can you tell me about a time when things didn’t go the way you wanted at work, such as a project that failed or being passed over for a promotion?',
         info:
-          'Unicorn vinyl poutine brooklyn, next level direct trade iceland. Shaman copper mug church-key coloring book, whatever poutine normcore fixie cred kickstarter post-ironic street art.',
+          'Everyone deals with professional setbacks at some point in their career. What you want to know is how people handled — and what they learned from — those situations. The best employees are resilient, using setbacks as a springboard toward positive changes. So listen to not only the problem they mention, but also what they did after the disappointment.',
       },
       {
         id: 7,
-        title: 'How do I change my My Page password?',
+        title: 'What are your favorite and least favorite technology products, and why?',
         info:
-          'Coloring book forage photo booth gentrify lumbersexual. Migas chillwave poutine synth shoreditch, enamel pin thundercats fashion axe roof party polaroid chartreuse.',
+          'In addition to learning whether prospective employees like the hardware, operating system and software your company uses, this tech interview question helps you evaluate enthusiasm and knowledge. Do candidates become animated when discussing the advantages and disadvantages of certain tools? Do they admire solid engineering, sleek design, intuitive user experience or another aspect of good technology? ',
       },
       {
         id: 8,
