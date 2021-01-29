736d1c727f707d9bd8b04e3b23d872dc8a6d95e6
John Trujillo <amriikk@gmail.com>
2021-01-29 00:44:19 -0800

    touch component + data files for project

---
+++ src/data.js
@@ -0,0 +1,93 @@
+const questions = [
+    {
+      id: 1,
+      title: 'What online resources do you use to help you do your job?',
+      info:
+        'Most IT workers turn to websites such as StackExchange or Github when they need help with something. Serious professionals will have their own selection of websites, online communities, social media feeds and other resources specific to their interests. The answer to this question will give you an indication of how engaged the candidate is with the broader IT world.  vinyl poutine brooklyn, next level direct trade iceland. Shaman copper mug church-key coloring book, whatever poutine normcore fixie cred kickstarter post-ironic street art.',
+    },
+    {
+      id: 2,
+      title: 'How do you keep your technology skills current?',
+      info:
+        'Coloring book forage photo booth gentrify lumbersexual. Migas chillwave poutine synth shoreditch, enamel pin thundercats fashion axe roof party polaroid chartreuse.',
+    },
+    {
+      id: 3,
+      title: 'What is BankID?',
+      info:
+        'Enamel pin fam sustainable woke whatever venmo. Authentic asymmetrical put a bird on it, lumbersexual activated charcoal kinfolk banjo cred pickled sartorial.',
+    },
+    {
+      id: 4,
+      title: 'Whose birth number can I use?',
+      info:
+        'Edison bulb direct trade gentrify beard lo-fi seitan sustainable roof party franzen occupy squid. Knausgaard cronut succulents, scenester readymade shabby chic lyft. Copper mug meh vegan gentrify.',
+    },
+    {
+      id: 5,
+      title: 'When do I recieve a password ordered by letter?',
+      info:
+        'Locavore franzen fashion axe live-edge neutra irony synth af tilde shabby chic man braid chillwave waistcoat copper mug messenger bag. Banjo snackwave blog, microdosing thundercats migas vaporware viral lo-fi seitan ',
+    },
+    {
+        id: 6,
+        title: 'Do I have to allow the use of cookies?',
+        info:
+          'Unicorn vinyl poutine brooklyn, next level direct trade iceland. Shaman copper mug church-key coloring book, whatever poutine normcore fixie cred kickstarter post-ironic street art.',
+      },
+      {
+        id: 7,
+        title: 'How do I change my My Page password?',
+        info:
+          'Coloring book forage photo booth gentrify lumbersexual. Migas chillwave poutine synth shoreditch, enamel pin thundercats fashion axe roof party polaroid chartreuse.',
+      },
+      {
+        id: 8,
+        title: 'What is BankID?',
+        info:
+          'Enamel pin fam sustainable woke whatever venmo. Authentic asymmetrical put a bird on it, lumbersexual activated charcoal kinfolk banjo cred pickled sartorial.',
+      },
+      {
+        id: 9,
+        title: 'Whose birth number can I use?',
+        info:
+          'Edison bulb direct trade gentrify beard lo-fi seitan sustainable roof party franzen occupy squid. Knausgaard cronut succulents, scenester readymade shabby chic lyft. Copper mug meh vegan gentrify.',
+      },
+      {
+        id: 10,
+        title: 'When do I recieve a password ordered by letter?',
+        info:
+          'Locavore franzen fashion axe live-edge neutra irony synth af tilde shabby chic man braid chillwave waistcoat copper mug messenger bag. Banjo snackwave blog, microdosing thundercats migas vaporware viral lo-fi seitan ',
+      },
+      {
+        id: 11,
+        title: 'Do I have to allow the use of cookies?',
+        info:
+          'Unicorn vinyl poutine brooklyn, next level direct trade iceland. Shaman copper mug church-key coloring book, whatever poutine normcore fixie cred kickstarter post-ironic street art.',
+      },
+      {
+        id: 12,
+        title: 'How do I change my My Page password?',
+        info:
+          'Coloring book forage photo booth gentrify lumbersexual. Migas chillwave poutine synth shoreditch, enamel pin thundercats fashion axe roof party polaroid chartreuse.',
+      },
+      {
+        id: 13,
+        title: 'What is BankID?',
+        info:
+          'Enamel pin fam sustainable woke whatever venmo. Authentic asymmetrical put a bird on it, lumbersexual activated charcoal kinfolk banjo cred pickled sartorial.',
+      },
+      {
+        id: 14,
+        title: 'Whose birth number can I use?',
+        info:
+          'Edison bulb direct trade gentrify beard lo-fi seitan sustainable roof party franzen occupy squid. Knausgaard cronut succulents, scenester readymade shabby chic lyft. Copper mug meh vegan gentrify.',
+      },
+      {
+        id: 15,
+        title: 'When do I recieve a password ordered by letter?',
+        info:
+          'Locavore franzen fashion axe live-edge neutra irony synth af tilde shabby chic man braid chillwave waistcoat copper mug messenger bag. Banjo snackwave blog, microdosing thundercats migas vaporware viral lo-fi seitan ',
+      },
+  ]
+  export default questions
